8c0e4cf9b2e57a42e8a66802711a17758ce3c5ca
Frederik Madsen <f.madsen@samsung.com>
2025-09-15 14:28:50 +0200

    Added silence.c

---
+++ silence.c
@@ -0,0 +1,53 @@
+//
+// gcc -shared -fPIC -o silence_ahbmrt.so silence_ahbmrt.c -ldl
+//
+// cp silence_ahbmrt.so ../src/cxl-gitlab.ssi.samsung.com/ahbm/ahbmpt/
+//
+// Run with LD_PRELOAD=./silence_ahbmrt.so, e.g:
+//
+// LD_PRELOAD=./silence_ahbmrt.so python example.py
+//
+
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <execinfo.h>
+#include <string.h>
+#include <stdarg.h>
+
+typedef int (*vprintf_t)(const char *fmt, va_list ap);
+typedef int (*vfprintf_t)(FILE *stream, const char *fmt, va_list ap);
+
+int printf(const char *fmt, ...)
+{
+    static vprintf_t real_vprintf = NULL;
+    static vfprintf_t real_vfprintf = NULL;
+    if (!real_vprintf)
+    {
+        real_vprintf = (vprintf_t)dlsym(RTLD_NEXT, "vprintf");
+        real_vfprintf = (vfprintf_t)dlsym(RTLD_NEXT, "vfprintf");
+    }
+
+    void *caller = __builtin_return_address(0);
+    Dl_info info;
+    if (dladdr(caller, &info) && info.dli_fname)
+    {
+        if (strstr(info.dli_fname, "libx.so"))
+        {
+            va_list args;
+            va_start(args, fmt);
+            FILE *logfile = fopen("libx.so.log", "a");
+            int ret = real_vfprintf(logfile, fmt, args);
+            fclose(logfile);
+            va_end(args);
+            return ret;
+        }
+    }
+
+    va_list args;
+    va_start(args, fmt);
+    int ret = real_vprintf(fmt, args);
+    va_end(args);
+    return ret;
+}
